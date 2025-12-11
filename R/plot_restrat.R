79a509c4d64d849d435e2343207bbe882e8a81e8
pete-hulson <pete.hulson@noaa.gov>
2025-12-10 15:01:28 -0900

    set up new plots

--- R/plot_restrat.R
+++ R/plot_restrat.R
@@ -21,7 +21,7 @@ plot_restrat <- function(index = NULL,
     dir.create(here::here('plots'))
   }
   
-  # get data prepped
+  # get data prepped ----
   index$og_indx_reg %>% 
     tidytable::filter(species_code %in% species) %>% 
     tidytable::left_join(index$new_indx_reg %>% 
@@ -45,100 +45,190 @@ plot_restrat <- function(index = NULL,
                       # cause i just like west to the left/top and east to the right/bottom
                       region = factor(region, levels = c("Western GOA", "Central GOA", "Eastern GOA", "GOA"))) -> db
   
-  # biomass timeseries plot
-  biomass <- db %>% 
-    tidytable::mutate(og_uci = biomass_mt + 1.96 * sqrt(biomass_var),
-                      og_lci = case_when(biomass_mt - 1.96 * sqrt(biomass_var) > 0 ~ biomass_mt - 1.96 * sqrt(biomass_var),
-                                         biomass_mt - 1.96 * sqrt(biomass_var) < 0 ~ 0)) %>% 
-    tidytable::select(year, region, Original = biomass_mt, Restratified = biom_new, uci = og_uci, lci = og_lci) %>% 
+  # subarea biomass time-series plot ----
+  biomass_dat <- db %>% 
+    tidytable::mutate(og_uci = case_when(year <= 2023 ~ biomass_mt + 1.96 * sqrt(biomass_var),
+                                         .default = 0),
+                      new_uci = case_when(year >= 2025 ~ biomass_mt + 1.96 * sqrt(biomass_var),
+                                          .default = 0),
+                      og_lci = case_when(year <= 2023 ~ case_when(biomass_mt - 1.96 * sqrt(biomass_var) > 0 ~ biomass_mt - 1.96 * sqrt(biomass_var),
+                                                                  biomass_mt - 1.96 * sqrt(biomass_var) < 0 ~ 0.1),
+                                         .default = 0),
+                      new_lci = case_when(year >= 2025 ~ case_when(biomass_mt - 1.96 * sqrt(biomass_var) > 0 ~ biomass_mt - 1.96 * sqrt(biomass_var),
+                                                                   biomass_mt - 1.96 * sqrt(biomass_var) < 0 ~ 0.1),
+                                          .default = 0)) %>% 
+    tidytable::select(year, region, Original = biomass_mt, Restratified = biom_new, og_uci, og_lci, new_uci, new_lci) %>% 
     tidytable::pivot_longer(., cols = c(Original, Restratified)) %>% 
-    tidytable::mutate(uci = case_when(name == 'Original' ~ uci,
-                                      name == 'Restratified' ~ value),
-                      lci = case_when(name == 'Original' ~ lci,
-                                      name == 'Restratified' ~ value)) %>% 
-    ggplot(aes(x = year, y = value, colour = name)) +
+    tidytable::mutate(uci = case_when(name == 'Original' & og_uci > 0 ~ og_uci,
+                                      name == 'Original' & og_uci == 0 ~ value,
+                                      name == 'Restratified' & new_uci > 0 ~ new_uci,
+                                      name == 'Restratified' & new_uci == 0 ~ value),
+                      lci = case_when(name == 'Original' & og_lci > 0 ~ og_lci,
+                                      name == 'Original' & og_lci == 0 ~ value,
+                                      name == 'Restratified' & new_lci > 0 ~ new_lci,
+                                      name == 'Restratified' & new_lci == 0 ~ value))
+  
+  biomass_dat %>% 
+    tidytable::filter(year < 2025) %>% 
+    # do 2025 swap
+    tidytable::bind_rows(biomass_dat %>% 
+                           tidytable::filter(year == 2025 & name == 'Original') %>% 
+                           tidytable::mutate(value = NA) %>% 
+                           tidytable::bind_rows(biomass_dat %>% 
+                                                  tidytable::filter(year == 2025 & name == 'Restratified') %>% 
+                                                  tidytable::select(-value) %>% 
+                                                  tidytable::left_join(biomass_dat %>% 
+                                                                         tidytable::filter(year == 2025 & name == 'Original') %>% 
+                                                                         tidytable::select(year, region, value)))) %>% 
+    tidytable::drop_na() %>% 
+    tidytable::select(-c(og_uci, og_lci, new_uci, new_lci)) -> biomass_dat
+  
+  biomass <- ggplot(data = biomass_dat %>% filter(region != 'GOA'), aes(x = year, y = value / 1000, colour = name, shape = name)) +
     geom_line(linewidth = 0.75, linetype = "dashed") +
-    geom_point() +
-    geom_pointrange(aes(ymin = lci, ymax = uci), linewidth = 0.75) +
+    geom_pointrange(aes(ymin = lci / 1000, ymax = uci / 1000), linewidth = 0.75, size = 0.5) +
     facet_wrap(~region, ncol = 1, scales = 'free_y') +
     theme_bw(base_size = 14) + 
-    labs(x = "Year", y = "Survey biomass (mt)", colour = "Index type", title = stock_name) +
+    labs(x = "Year", y = "Survey biomass (1000s mt)", colour = "Index type", shape = "Index type", title = stock_name) +
     scico::scale_color_scico_d(palette = 'roma')
   
-  ggsave(filename = here::here('plots', paste0(stock_name, '_biom_ts.png')),
-         plot = biomass)
+  ggsave(filename = here::here('plots', paste0(stock_name, '_subreg_biom.png')),
+         plot = biomass,
+         width = 11,
+         height = 7,
+         units = "in")
   
-  # numbers timeseries plot
-  numbers <- db %>% 
-    tidytable::mutate(og_uci = population_count + 1.96 * sqrt(population_var),
-                      og_lci = case_when(population_count - 1.96 * sqrt(population_var) > 0 ~ population_count - 1.96 * sqrt(population_var),
-                                         population_count - 1.96 * sqrt(population_var) < 0 ~ 0)) %>% 
-    tidytable::select(year, region, Original = population_count, Restratified = popn_new, uci = og_uci, lci = og_lci) %>% 
+  # numbers time-series plot ----
+  numbers_dat <- db %>% 
+    tidytable::mutate(og_uci = case_when(year <= 2023 ~ population_count + 1.96 * sqrt(population_var),
+                                         .default = 0),
+                      new_uci = case_when(year >= 2025 ~ population_count + 1.96 * sqrt(population_var),
+                                          .default = 0),
+                      og_lci = case_when(year <= 2023 ~ case_when(population_count - 1.96 * sqrt(population_var) > 0 ~ population_count - 1.96 * sqrt(population_var),
+                                                                  population_count - 1.96 * sqrt(population_var) < 0 ~ 0.1),
+                                         .default = 0),
+                      new_lci = case_when(year >= 2025 ~ case_when(population_count - 1.96 * sqrt(population_var) > 0 ~ population_count - 1.96 * sqrt(population_var),
+                                                                   population_count - 1.96 * sqrt(population_var) < 0 ~ 0.1),
+                                          .default = 0)) %>% 
+    tidytable::select(year, region, Original = population_count, Restratified = popn_new, og_uci, og_lci, new_uci, new_lci) %>% 
     tidytable::pivot_longer(., cols = c(Original, Restratified)) %>% 
-    tidytable::mutate(uci = case_when(name == 'Original' ~ uci,
-                                      name == 'Restratified' ~ value),
-                      lci = case_when(name == 'Original' ~ lci,
-                                      name == 'Restratified' ~ value)) %>% 
-    ggplot(aes(x = year, y = value, colour = name)) +
+    tidytable::mutate(uci = case_when(name == 'Original' & og_uci > 0 ~ og_uci,
+                                      name == 'Original' & og_uci == 0 ~ value,
+                                      name == 'Restratified' & new_uci > 0 ~ new_uci,
+                                      name == 'Restratified' & new_uci == 0 ~ value),
+                      lci = case_when(name == 'Original' & og_lci > 0 ~ og_lci,
+                                      name == 'Original' & og_lci == 0 ~ value,
+                                      name == 'Restratified' & new_lci > 0 ~ new_lci,
+                                      name == 'Restratified' & new_lci == 0 ~ value))
+  
+  numbers_dat %>% 
+    tidytable::filter(year < 2025) %>% 
+    # do 2025 swap
+    tidytable::bind_rows(numbers_dat %>% 
+                           tidytable::filter(year == 2025 & name == 'Original') %>% 
+                           tidytable::mutate(value = NA) %>% 
+                           tidytable::bind_rows(numbers_dat %>% 
+                                                  tidytable::filter(year == 2025 & name == 'Restratified') %>% 
+                                                  tidytable::select(-value) %>% 
+                                                  tidytable::left_join(numbers_dat %>% 
+                                                                         tidytable::filter(year == 2025 & name == 'Original') %>% 
+                                                                         tidytable::select(year, region, value)))) %>% 
+    tidytable::drop_na() %>% 
+    tidytable::select(-c(og_uci, og_lci, new_uci, new_lci)) -> numbers_dat
+    
+  numbers <- ggplot(data = numbers_dat %>% filter(region != 'GOA'), aes(x = year, y = value / 1000000, colour = name, shape = name)) +
     geom_line(linewidth = 0.75, linetype = "dashed") +
-    geom_point() +
-    geom_pointrange(aes(ymin = lci, ymax = uci), linewidth = 0.75) +
+    geom_pointrange(aes(ymin = lci / 1000000, ymax = uci / 1000000), linewidth = 0.75, size = 0.5) +
     facet_wrap(~region, ncol = 1, scales = 'free_y') +
     theme_bw(base_size = 14) + 
-    labs(x = "Year", y = "Survey numbers", colour = "Index type", title = stock_name) +
+    labs(x = "Year", y = "Survey numbers (millions)", colour = "Index type", shape = "Index type", title = stock_name) +
     scico::scale_color_scico_d(palette = 'roma')
   
-  ggsave(filename = here::here('plots', paste0(stock_name, '_num_ts.png')),
-         plot = numbers)
+  ggsave(filename = here::here('plots', paste0(stock_name, '_subreg_num.png')),
+         plot = numbers,
+         width = 11,
+         height = 7,
+         units = "in")
   
-  # % difference in biomass plot
-  db %>% 
-    tidytable::select(year, region, diff_biom, biom_test) %>% 
-    tidytable::mutate(perc_diff = scales::percent(diff_biom / 100, accuracy = 1, trim = FALSE),
-                      pjust = case_when(diff_biom > 0 ~ -0.5,
-                                        diff_biom < 0 ~ 1),
-                      biom_test = case_when(biom_test == 0 ~ "plain",
-                                            biom_test == 1 ~ "bold")) -> plot_dat
   
-  biomass_diff <- ggplot(plot_dat, aes(x = year, y = diff_biom, fill = region)) +
-    geom_bar(stat = "identity") +
-    geom_text(aes(label = perc_diff, vjust = pjust, fontface = biom_test)) +
-    facet_wrap(~region, ncol = 1, scales = 'free_y') +
-    ylim(floor(1.2 * min(plot_dat$diff_biom)), ceiling(1.2 * max(plot_dat$diff_biom))) +
-    geom_abline(intercept = 0, slope = 0, color = "black") +
-    scico::scale_fill_scico_d(palette = 'roma') +
+  # biomass & numbers time-series plot ----
+  
+  biomass_dat %>% 
+    filter(region == 'GOA') %>% 
+    tidytable::mutate(index = 'Biomass (1000s mt)',
+                      value = value / 1000,
+                      uci = uci / 1000,
+                      lci = lci / 1000) %>% 
+    tidytable::bind_rows(numbers_dat %>% 
+                           filter(region == 'GOA') %>% 
+                           tidytable::mutate(index = 'Numbers (millions)',
+                                             value = value / 1000000,
+                                             uci = uci / 1000000,
+                                             lci = lci / 1000000)) -> num_biom_dat
+  
+  num_biom <- ggplot(data = num_biom_dat, aes(x = year, y = value, colour = name, shape = name)) +
+    geom_line(linewidth = 0.75, linetype = "dashed") +
+    geom_pointrange(aes(ymin = lci, ymax = uci), linewidth = 0.75, size = 0.5) +
+    facet_wrap(~index, ncol = 1, scales = 'free_y') +
     theme_bw(base_size = 14) + 
-    labs(x = "Year", y = "Percent difference in biomass after restratification", fill = "Region", title = stock_name)
+    labs(x = "Year", y = "Survey index", colour = "Index type", shape = "Index type", title = stock_name) +
+    scico::scale_color_scico_d(palette = 'roma')
+
+  ggsave(filename = here::here('plots', paste0(stock_name, '_num_biom_ts.png')),
+         plot = num_biom,
+         width = 11,
+         height = 7,
+         units = "in")
   
-  ggsave(filename = here::here('plots', paste0(stock_name, '_biom_diff.png')),
-         plot = biomass_diff)
-  
-  # % difference in numbers plot
-  db %>% 
-    tidytable::select(year, region, diff_popn, popn_test) %>% 
-    tidytable::mutate(perc_diff = scales::percent(diff_popn / 100, accuracy = 1, trim = FALSE),
-                      pjust = case_when(diff_popn > 0 ~ -0.5,
-                                        diff_popn < 0 ~ 1),
-                      biom_test = case_when(popn_test == 0 ~ "plain",
-                                            popn_test == 1 ~ "bold")) -> plot_dat
-  
-  numbers_diff <- ggplot(plot_dat, aes(x = year, y = diff_popn, fill = region)) +
-    geom_bar(stat = "identity") +
-    geom_text(aes(label = perc_diff, vjust = pjust, fontface = biom_test)) +
-    facet_wrap(~region, ncol = 1, scales = 'free_y') +
-    ylim(floor(1.2 * min(plot_dat$diff_popn)), ceiling(1.2 * max(plot_dat$diff_popn))) +
-    geom_abline(intercept = 0, slope = 0, color = "black") +
-    scico::scale_fill_scico_d(palette = 'roma') +
-    theme_bw(base_size = 14) + 
-    labs(x = "Year", y = "Percent difference in numbers after restratification", fill = "Region", title = stock_name)
-  
-  ggsave(filename = here::here('plots', paste0(stock_name, '_num_diff.png')),
-         plot = numbers_diff)
+  # # % difference in biomass plot
+  # db %>% 
+  #   tidytable::select(year, region, diff_biom, biom_test) %>% 
+  #   tidytable::mutate(perc_diff = scales::percent(diff_biom / 100, accuracy = 1, trim = FALSE),
+  #                     pjust = case_when(diff_biom > 0 ~ -0.5,
+  #                                       diff_biom < 0 ~ 1),
+  #                     biom_test = case_when(biom_test == 0 ~ "plain",
+  #                                           biom_test == 1 ~ "bold")) -> plot_dat
+  # 
+  # biomass_diff <- ggplot(plot_dat, aes(x = year, y = diff_biom, fill = region)) +
+  #   geom_bar(stat = "identity") +
+  #   geom_text(aes(label = perc_diff, vjust = pjust, fontface = biom_test)) +
+  #   facet_wrap(~region, ncol = 1, scales = 'free_y') +
+  #   ylim(floor(1.2 * min(plot_dat$diff_biom)), ceiling(1.2 * max(plot_dat$diff_biom))) +
+  #   geom_abline(intercept = 0, slope = 0, color = "black") +
+  #   scico::scale_fill_scico_d(palette = 'roma') +
+  #   theme_bw(base_size = 14) + 
+  #   labs(x = "Year", y = "Percent difference in biomass after restratification", fill = "Region", title = stock_name)
+  # 
+  # ggsave(filename = here::here('plots', paste0(stock_name, '_biom_diff.png')),
+  #        plot = biomass_diff,
+  #        width = 11,
+  #        height = 7,
+  #        units = "in")
+  # 
+  # # % difference in numbers plot
+  # db %>% 
+  #   tidytable::select(year, region, diff_popn, popn_test) %>% 
+  #   tidytable::mutate(perc_diff = scales::percent(diff_popn / 100, accuracy = 1, trim = FALSE),
+  #                     pjust = case_when(diff_popn > 0 ~ -0.5,
+  #                                       diff_popn < 0 ~ 1),
+  #                     biom_test = case_when(popn_test == 0 ~ "plain",
+  #                                           popn_test == 1 ~ "bold")) -> plot_dat
+  # 
+  # numbers_diff <- ggplot(plot_dat, aes(x = year, y = diff_popn, fill = region)) +
+  #   geom_bar(stat = "identity") +
+  #   geom_text(aes(label = perc_diff, vjust = pjust, fontface = biom_test)) +
+  #   facet_wrap(~region, ncol = 1, scales = 'free_y') +
+  #   ylim(floor(1.2 * min(plot_dat$diff_popn)), ceiling(1.2 * max(plot_dat$diff_popn))) +
+  #   geom_abline(intercept = 0, slope = 0, color = "black") +
+  #   scico::scale_fill_scico_d(palette = 'roma') +
+  #   theme_bw(base_size = 14) + 
+  #   labs(x = "Year", y = "Percent difference in numbers after restratification", fill = "Region", title = stock_name)
+  # 
+  # ggsave(filename = here::here('plots', paste0(stock_name, '_num_diff.png')),
+  #        plot = numbers_diff)
   
   # return plot output
-  list(biomass = biomass,
-       numbers = numbers,
-       biomass_diff = biomass_diff,
-       numbers_diff = numbers_diff)
+  # list(biomass = biomass,
+  #      numbers = numbers,
+  #      num_biom = num_biom)
 
 }
