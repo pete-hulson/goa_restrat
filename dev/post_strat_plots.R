6bbb1f58d975452ab5e33bf512855eb7a499cc48
pete-hulson <pete.hulson@noaa.gov>
2026-03-04 15:07:34 -0900

    construct app to see restrat figs

---
+++ dev/post_strat_plots.R
@@ -0,0 +1,217 @@
+library(tidyverse)
+library(cowplot)
+
+all_ests <- vroom::vroom(here::here('output', 'all_ests.csv'))
+sp_names <- vroom::vroom(here::here('data', 'species_names.csv'))
+
+# tier 3 species codes
+species_t3 = c(10110, 10130, 10180, 21720, 21740, 30060, 30420, 10261, 10262, 10200)
+species_rebs = c(30050, 30051, 30052)
+species_dusky = c(30150, 30152)
+
+# tier 5 species codes
+species_t5 = c(30020, 30576)
+species_orox = c(30100, 30430, 30475, 30535, 30560)
+species_swf = c(10285, 10270, 10170, 10250, 10220, 10210)
+
+# put 'em together
+species = c(species_t3, species_t5, species_rebs, species_dusky, species_orox, species_swf)
+
+# plot regional and subregional indices for stocks with single species code
+all_ests %>% 
+  dplyr::rename_all(tolower) %>% 
+  tidytable::filter(species_code %in% species) %>% 
+  tidytable::left_join(sp_names) -> plot_dat
+
+for(i in 1:length(c(species_t3, species_t5))){
+  
+  # get plot data together
+  ispp <- c(species_t3, species_t5)[i]
+  dat <- plot_dat %>% 
+    tidytable::filter(region == 'GOA' & species_code == ispp)
+  dat_subreg <- plot_dat %>% 
+    tidytable::filter(region != 'GOA' & species_code == ispp) %>% 
+    tidytable::mutate(region = factor(region, levels = c("Western GOA", "Central GOA", "Eastern GOA")))
+  stock_name <- paste0(first(dat$species_name), " (",first(dat$common_name), ")")
+  comm_name <- first(dat$common_name)
+  
+  # biomass index
+  biomass <- ggplot(data = dat, 
+    aes(x = year, 
+        y = biomass_mt * 1e-3, 
+        colour = est_type, shape = est_type)) +
+    geom_line(linewidth = 0.75, linetype = "dashed") +
+    geom_pointrange(aes(ymin = biomass_lci * 1e-3, 
+                        ymax = biomass_hci * 1e-3), 
+                    linewidth = 0.75, size = 0.5) +
+    theme_bw(base_size = 14) + 
+    labs(x = "", y = "Survey biomass (thousand mt)", 
+         colour = "Index type", shape = "Index type", title = stock_name) +
+    scico::scale_color_scico_d(palette = 'roma')
+  
+  # numbers index
+  num <- ggplot(data = dat, 
+    aes(x = year, 
+        y = population_count * 1e-6, 
+        colour = est_type, shape = est_type)) +
+    geom_line(linewidth = 0.75, linetype = "dashed") +
+    geom_pointrange(aes(ymin = population_lci * 1e-6, 
+                        ymax = population_hci * 1e-6), 
+                    linewidth = 0.75, size = 0.5) +
+    theme_bw(base_size = 14) + 
+    labs(x = "Year", y = "Survey numbers (millions)", 
+         colour = "Index type", 
+         shape = "Index type") +
+    scico::scale_color_scico_d(palette = 'roma')
+
+  # save plots
+  ggsave(filename = here::here('survey_comp_app', 'www', 'stocks',
+                               paste0(gsub(x = comm_name, 
+                                           pattern = " ", 
+                                           replacement = "_"), 
+                                      '_region.png')),
+         plot =   cowplot::plot_grid(biomass, num, ncol = 1),
+         width = 11, height = 7, units = "in")
+  
+  # biomass by subregion
+  biomass_subreg <- ggplot(data = dat_subreg, 
+    aes(x = year, 
+        y = biomass_mt / 1000, 
+        colour = est_type, 
+        shape = est_type)) +
+    geom_line(linewidth = 0.75, linetype = "dashed") +
+    geom_pointrange(aes(ymin = biomass_lci / 1000, 
+                        ymax = biomass_hci  / 1000), 
+                    linewidth = 0.75, 
+                    size = 0.5) +
+    facet_wrap(facets = ~region, 
+               ncol = 1, 
+               scales = 'free_y') +
+    theme_bw(base_size = 14) + 
+    labs(x = "Year", y = "Survey biomass (1000s mt)", 
+         colour = "Index type", 
+         shape = "Index type", 
+         title = stock_name) +
+    scico::scale_color_scico_d(palette = 'roma')
+  
+  # save plot
+  ggsave(filename = here::here('survey_comp_app', 'www', 'stocks',
+                               paste0(gsub(x = comm_name, 
+                                           pattern = " ", 
+                                           replacement = "_"), 
+                                      '_subreg_biom.png')),
+         plot = biomass_subreg,
+         width = 11, height = 7, units = "in")
+  
+}
+
+# plot for stock complexes
+complex_species <- list(species_rebs,
+                        species_dusky,
+                        species_orox,
+                        species_swf)
+
+complex_names <- c('Rougheye-Blackspotted rockfish',
+                   'Dusky rockfish',
+                   'Other rockfish',
+                   'Shallow-water flatfish')
+
+for(i in 1:length(complex_species)){
+  
+  # get plot data together
+  ispp <- complex_species[[i]]
+  dat <-   plot_dat %>% 
+    tidytable::filter(region == 'GOA' & species_code %in% ispp) %>% 
+    tidytable::summarise(biomass_mt = sum(biomass_mt),
+                         biomass_var = sum(((biomass_hci - biomass_mt) / 1.96)^2),
+                         population_count = sum(population_count),
+                         popn_var = sum(((population_hci - population_count) / 1.96)^2),
+                         .by = c(year, est_type, region)) %>% 
+    tidytable::mutate(biomass_hci = biomass_mt + 1.96 * sqrt(biomass_var),
+                      biomass_lci = biomass_mt - 1.96 * sqrt(biomass_var),
+                      population_hci = population_count + 1.96 * sqrt(popn_var),
+                      population_lci = population_count - 1.96 * sqrt(popn_var))
+  dat_subreg <- plot_dat %>% 
+    tidytable::filter(region != 'GOA' & species_code %in% ispp) %>% 
+    tidytable::summarise(biomass_mt = sum(biomass_mt),
+                         biomass_var = sum(((biomass_hci - biomass_mt) / 1.96)^2),
+                         population_count = sum(population_count),
+                         popn_var = sum(((population_hci - population_count) / 1.96)^2),
+                         .by = c(year, est_type, region)) %>% 
+    tidytable::mutate(biomass_hci = biomass_mt + 1.96 * sqrt(biomass_var),
+                      biomass_lci = biomass_mt - 1.96 * sqrt(biomass_var),
+                      population_hci = population_count + 1.96 * sqrt(popn_var),
+                      population_lci = population_count - 1.96 * sqrt(popn_var)) %>% 
+    tidytable::mutate(region = factor(region, levels = c("Western GOA", "Central GOA", "Eastern GOA")))
+  
+  complex_name <- complex_names[i]
+  
+  # biomass index
+  biomass <- ggplot(data = dat, 
+                    aes(x = year, 
+                        y = biomass_mt * 1e-3, 
+                        colour = est_type, shape = est_type)) +
+    geom_line(linewidth = 0.75, linetype = "dashed") +
+    geom_pointrange(aes(ymin = biomass_lci * 1e-3, 
+                        ymax = biomass_hci * 1e-3), 
+                    linewidth = 0.75, size = 0.5) +
+    theme_bw(base_size = 14) + 
+    labs(x = "", y = "Survey biomass (thousand mt)", 
+         colour = "Index type", shape = "Index type", title = complex_name) +
+    scico::scale_color_scico_d(palette = 'roma')
+  
+  # numbers index
+  num <- ggplot(data = dat, 
+                aes(x = year, 
+                    y = population_count * 1e-6, 
+                    colour = est_type, shape = est_type)) +
+    geom_line(linewidth = 0.75, linetype = "dashed") +
+    geom_pointrange(aes(ymin = population_lci * 1e-6, 
+                        ymax = population_hci * 1e-6), 
+                    linewidth = 0.75, size = 0.5) +
+    theme_bw(base_size = 14) + 
+    labs(x = "Year", y = "Survey numbers (millions)", 
+         colour = "Index type", 
+         shape = "Index type") +
+    scico::scale_color_scico_d(palette = 'roma')
+  
+  # save plots
+  ggsave(filename = here::here('survey_comp_app', 'www', 'stocks',
+                               paste0(gsub(x = complex_name, 
+                                           pattern = " ", 
+                                           replacement = "_"), 
+                                      '_region.png')),
+         plot =   cowplot::plot_grid(biomass, num, ncol = 1),
+         width = 11, height = 7, units = "in")
+  
+  # biomass by subregion
+  biomass_subreg <- ggplot(data = dat_subreg, 
+                           aes(x = year, 
+                               y = biomass_mt / 1000, 
+                               colour = est_type, 
+                               shape = est_type)) +
+    geom_line(linewidth = 0.75, linetype = "dashed") +
+    geom_pointrange(aes(ymin = biomass_lci / 1000, 
+                        ymax = biomass_hci  / 1000), 
+                    linewidth = 0.75, 
+                    size = 0.5) +
+    facet_wrap(facets = ~region, 
+               ncol = 1, 
+               scales = 'free_y') +
+    theme_bw(base_size = 14) + 
+    labs(x = "Year", y = "Survey biomass (1000s mt)", 
+         colour = "Index type", 
+         shape = "Index type", 
+         title = complex_name) +
+    scico::scale_color_scico_d(palette = 'roma')
+  
+  # save plot
+  ggsave(filename = here::here('survey_comp_app', 'www', 'stocks',
+                               paste0(gsub(x = complex_name, 
+                                           pattern = " ", 
+                                           replacement = "_"), 
+                                      '_subreg_biom.png')),
+         plot = biomass_subreg,
+         width = 11, height = 7, units = "in")
+}
+
---
+++ survey_comp_app/app.R
@@ -0,0 +1,92 @@
+library(tidytable)
+library(shiny)
+library(stringr)
+
+# 1. Setup - Let's assume your figures are in a folder named 'www/stocks'
+# It is best practice to put images in a 'www' subfolder for Shiny
+
+file_list <- list.files(file.path('www', 'stocks'), pattern = "\\.png$|\\.jpg$")
+
+# Extract stock names from filenames
+stock_names <- str_remove(file_list, "_subreg_biom*\\.png$|_region*\\.png$") %>% 
+  unique() %>% 
+  gsub(pattern = "_", 
+       replacement = " ")
+
+
+ui <- fluidPage(
+  # The title
+  fluidRow(
+    column(12, 
+           h2("Gulf of Alaska bottom trawl survey design comparison", 
+              style = "text-align: center; font-weight: bold;")
+    )
+  ),
+  
+  # The explanatory text
+  fluidRow(
+    column(12, 
+           p("Herein contains figures that compare the historical index estimates from the GOA bottom trawl survey to the index estimates from the survey design implemented in 2025.
+             Figures are included for each Tier 3 and 5 stock assessed by the AFSC within the GOA.",
+             style = "font-size: 14px;")
+    )
+  ),
+  
+  # The Dropdown Menu
+  fluidRow(
+    column(width = 4, offset = 4, # Centers the dropdown (4 columns wide, pushed 4 from left)
+           selectInput("stock_select", "Choose a Stock:", 
+                       choices = stock_names, 
+                       width = "100%")
+    )
+  ),
+  
+  hr(style = "border-top: 2px solid #222;"), # Adds a horizontal line for visual separation
+  
+  # The Plots and Text
+  fluidRow(
+    column(width = 10, offset = 1, # Keeps the main content slightly narrower for readability
+           p(strong('Survey indices'), style = "text-align: center; font-size: 21px;"),
+           p('GOA bottom trawl survey biomass (top) and population (bottom) index time series from the original design-based estimates (ORIG) and the post-stratified design-based estimates (PS) under the new survey design.'),
+           imageOutput("figure1", height = "auto"),
+           
+           br(), # Adds extra vertical space
+           hr(style = "border-top: 2px solid #222;"), # Adds a horizontal line for visual separation
+           
+           p(strong('Survey subregion biomass indices'), style = "text-align: center; font-size: 21px;"),
+           p('GOA bottom trawl survey biomass time series by sebregion from the original design-based estimates (ORIG) and the post-stratified design-based estimates (PS) under the new survey design.'),
+           imageOutput("figure2", height = "auto")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  # Function to find and render the image
+  output$figure1 <- renderImage({
+    # Build path relative to the app.R file
+    path <- file.path("www", "stocks", 
+                      paste0(gsub(input$stock_select, pattern = " ", replacement = "_"), 
+                             "_region.png"))
+    
+    list(src = path,
+         width = "100%",
+         alt = paste("Survey index plot for", input$stock_select))
+  }, deleteFile = FALSE)
+  
+  output$figure2 <- renderImage({
+    # Build path relative to the app.R file
+    path <- file.path("www", "stocks", 
+                      paste0(gsub(input$stock_select,
+                                  pattern = " ", 
+                                  replacement = "_"), 
+                             "_subreg_biom.png"))
+    
+    list(src = path,
+         width = "100%",
+         alt = paste("Subregion biomass plot for", input$stock_select))
+  }, deleteFile = FALSE)
+  
+}
+
+shinyApp(ui, server)
